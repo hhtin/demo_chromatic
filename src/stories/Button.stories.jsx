41d08aabcc453ffe2b28fd0fd0069c87ee03dd9d
hhtin <tinhuynhhuu250500@gmail.com>
2022-11-10 16:53:28 +0700

    test

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,45 +1,70 @@
+// import React from "react";
+
+// import { Button } from "./Button";
+
+// // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// export default {
+//   title: "Example/Button",
+//   component: Button,
+//   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+//   argTypes: {
+//     backgroundColor: { control: "color" },
+//   },
+// };
+
+// // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// const Template = (args) => <Button {...args} />;
+
+// export const Primary = Template.bind({});
+// // More on args: https://storybook.js.org/docs/react/writing-stories/args
+// Primary.args = {
+//   primary: true,
+//   label: "Test",
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: "ABCDAA",
+// };
+
+// export const Thirds = Template.bind({});
+// Thirds.args = {
+//   label: "ABCDAA",
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: "large",
+//   label: "Button",
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: "small",
+//   label: "Button",
+// };
+
+// Button.stories.js|jsx
+
 import React from "react";
 
 import { Button } from "./Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Button",
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
+  //👇 Creates specific parameters for the story
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: "red", value: "#f00" },
+        { name: "green", value: "#0f0" },
+        { name: "blue", value: "#00f" },
+      ],
+    },
   },
-};
-
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: "Test",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "ABCDAA",
-};
-
-export const Thirds = Template.bind({});
-Thirds.args = {
-  label: "ABCDAA",
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
-  label: "Button",
 };
